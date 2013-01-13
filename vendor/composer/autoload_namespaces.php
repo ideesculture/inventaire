880093df777715db1538cb4e3354b6fa16a00eb6
Gautier Michelin <gm@ideesculture.com>
2013-01-13 10:04:45 +0100

    dev
    dev

--- vendor/composer/autoload_namespaces.php
+++ vendor/composer/autoload_namespaces.php
@@ -12,5 +12,6 @@ return array(
     'ZendTest\\' => $vendorDir . '/zendframework/zendframework/tests/',
     'Doctrine\\Common\\' => $vendorDir . '/doctrine/common/lib/',
     'DluTwBootstrap' => $vendorDir . '/dlu/dlutwbootstrap/src/',
+    'Doctrine\\Common' => $vendorDir . '/doctrine/common/lib/',
     'DOMPDFModule' => $vendorDir . '/dino/dompdf-module/src/',
 );
